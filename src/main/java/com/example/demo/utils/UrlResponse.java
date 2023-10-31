43f780880011becdca15106578825682321b9f8d
AlejandraPardo9235 <113046836+AlejandraPardo9235@users.noreply.github.com>
2023-10-30 21:31:07 -0500

    commit urlResponse

---
+++ src/main/java/com/example/demo/utils/UrlResponse.java
@@ -0,0 +1,25 @@
+package com.example.demo.utils;
+
+public class UrlResponse {
+
+	private String url;
+	
+	public UrlResponse(String url) {
+		this.url = url;
+	}
+	
+	public UrlResponse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
+	
+}
